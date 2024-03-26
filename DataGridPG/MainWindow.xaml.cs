adc11bc4bc36684dfb104185b91188ffee4f7cab
lofti198 <isolar2005@gmail.com>
2024-03-26 06:16:52 +0200

    Add project files.

---
+++ DataGridPG/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridPG;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MessageBoxButton_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Hello");
+    }
+
+    private async void FillTableButton_Click(object sender, RoutedEventArgs e)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        DataTable dt = await Utilities.GenerateDataTableWithNRowsAsync(3000000);
+        
+        Debug.WriteLine($"Generating DataTable took: {stopwatch.ElapsedMilliseconds}ms");
+
+        stopwatch.Restart();
+
+        dataGrid.ItemsSource = dt.DefaultView;
+        
+        Debug.WriteLine($"Setting ItemsSource took: {stopwatch.ElapsedMilliseconds}ms");
+
+    }
+}
---
+++ DataGridPG/Utilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DataGridPG
+{
+    public static class Utilities
+    {
+        public static async Task<DataTable> GenerateDataTableWithNRowsAsync(int N)
+        {
+            return await Task.Run(() => GenerateDataTableWithNRows(N));
+
+        }
+        public static DataTable GenerateDataTableWithNRows(int N)
+        {
+            // Create a new DataTable object
+            DataTable dataTable = new DataTable();
+
+            // Add columns to the DataTable
+            dataTable.Columns.Add("ID", typeof(int));
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("Beruf", typeof(string));
+
+            // Add N rows to the DataTable
+            for (int i = 1; i <= N; i++)
+            {
+                // Each row can have default or sequential values. Here we use sequential for demonstration.
+                dataTable.Rows.Add(i, $"Name {i}", $"Beruf {i}");
+            } 
+
+            return dataTable;
+        }
+
+    }
+}
